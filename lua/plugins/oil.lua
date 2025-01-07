e7da679a194985d935d88523c2a7b74fc8b3004b
tomcotter7 <thomascotter00@gmail.com>
2025-01-07 07:53:08 +0000

    Added .gitignore tracking to oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,6 +6,5 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-    require('oil').setup({})
   end
 }
