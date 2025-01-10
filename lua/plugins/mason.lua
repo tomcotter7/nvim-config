f7bec2d5a8b45ae9c3ff24bfdbb3bc70a9b73c6d
tomcotter7 <thomascotter00@gmail.com>
2025-01-10 13:54:06 +0000

    groovyls

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,9 +2,13 @@ return {
   "williamboman/mason.nvim",
   dependencies = { "williamboman/mason-lspconfig.nvim" },
   config = function()
-    require('mason').setup({})
+    require('mason').setup({
+      ui = {
+        border = "rounded"
+      }
+    })
     require('mason-lspconfig').setup({
-      ensure_installed = { 'lua_ls', 'ruff', 'pyright', 'ts_ls', 'terraformls' }
+      ensure_installed = { 'lua_ls', 'ruff', 'pyright', 'ts_ls', 'terraformls', 'texlab', 'groovyls' }
     })
     local lspconfig = require('lspconfig')
     lspconfig.ts_ls.setup({})
@@ -12,5 +16,7 @@ return {
     lspconfig.ruff.setup({})
     lspconfig.lua_ls.setup({})
     lspconfig.terraformls.setup({})
+    lspconfig.texlab.setup({})
+    lspconfig.groovyls.setup({})
   end
 }
