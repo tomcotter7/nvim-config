ce627441d3370e9b52fbe3056bb2eb6387b79f54
tomcotter7 <thomascotter00@gmail.com>
2025-04-01 09:42:10 +0100

    fixed cmp version while broken code exists

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,24 +1,30 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-nvim-lsp" },
-    config = function()
-        local cmp = require('cmp')
-        cmp.setup({
-            sources = {
-                    {name = 'nvim_lsp'}
-            },
-            window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-            },
-            mapping = cmp.mapping.preset.insert({
-                    ['<C-p>'] = cmp.mapping.select_prev_item(),
-                    ['<C-n>'] = cmp.mapping.select_next_item(),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-f>'] = cmp.mapping.scroll_docs(-4),
-                    ['<C-y>'] = cmp.mapping.confirm({select = true}),
-                    ['<C-Space>'] = cmp.mapping.complete(),
-            })
-        })
-    end
+  "hrsh7th/nvim-cmp",
+  commit = "1e1900b",
+  dependencies = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/vim-vsnip" },
+  config = function()
+    local cmp = require('cmp')
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          vim.fn["vsnip#anonymous"](args.body)
+        end,
+      },
+      sources = {
+        { name = 'nvim_lsp' }
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+      mapping = cmp.mapping.preset.insert({
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-Space>'] = cmp.mapping.complete(),
+      })
+    })
+  end
 }
