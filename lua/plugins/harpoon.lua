29a310571c7e7dbb1f992f115ad4fb8b625592e4
Tom Cotter <thomascotter00@gmail.com>
2025-06-16 16:41:11 +0100

    fix: updates to harpoon package

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,6 +8,16 @@ return {
 
     local conf = require("telescope.config").values
     local function toggle_telescope(harpoon_files)
+      local finder = function()
+        local paths = {}
+        for _, item in ipairs(harpoon_files.items) do
+          table.insert(paths, item.value)
+        end
+
+        return require("telescope.finders").new_table({
+          results = paths,
+        })
+      end
       local file_paths = {}
       for _, item in ipairs(harpoon_files.items) do
         table.insert(file_paths, item.value)
@@ -20,11 +30,22 @@ return {
         }),
         previewer = conf.file_previewer({}),
         sorter = conf.generic_sorter({}),
+        attach_mappings = function(prompt_bufnr, map)
+          map("i", "<C-d>", function()
+            local state = require("telescope.actions.state")
+            local selected_entry = state.get_selected_entry()
+            local current_picker = state.get_current_picker(prompt_bufnr)
+
+            table.remove(harpoon_files.items, selected_entry.index)
+            current_picker:refresh(finder())
+          end)
+          return true
+        end,
       }):find()
     end
 
-    vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-    vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end, { desc = "harpoon (a)dd" })
+    vim.keymap.set("n", "<leader>hf", function() toggle_telescope(harpoon:list()) end,
       { desc = "Open harpoon window" })
 
     vim.keymap.set("n", "<leader>h1", function() harpoon:list():select(1) end)
