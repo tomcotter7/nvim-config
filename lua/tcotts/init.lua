03539a346a940c71f688f3710741f3a1e0fe55da
Tom Cotter <thomascotter00@gmail.com>
2025-08-01 10:50:22 -0400

    git in airline

--- lua/tcotts/init.lua
+++ lua/tcotts/init.lua
@@ -2,6 +2,8 @@ require("tcotts.set")
 require("tcotts.remap")
 require("config.lazy")
 
+vim.g['airline#extensions#branch#enabled'] = 1
+
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
   command = "AirlineTheme violet",
 })
