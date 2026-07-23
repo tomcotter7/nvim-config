7f3316f4f426c2d50bcf013d16ca53601f26d863
Tom Cotter <thomascotter00@gmail.com>
2026-07-23 17:31:31 +0100

    refactor: deleting unused plugins

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -18,13 +18,3 @@ return {
     end
   }
 }
-
--- return {
---   {
---     "github/copilot.vim",
---     config = function()
---       vim.g.copilot_enabled = true
---       vim.g.copilot_filetypes = { markdown = false }
---     end,
---   },
--- }
